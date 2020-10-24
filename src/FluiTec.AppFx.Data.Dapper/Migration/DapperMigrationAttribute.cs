20d6af99c2b3657d4d128b913cc5146c895e25e3
Achim Schnell <a.schnell@wtschnell.de>
2020-10-24 20:16:16 +0200

    try fixing value calculation

--- src/FluiTec.AppFx.Data.Dapper/Migration/DapperMigrationAttribute.cs
+++ src/FluiTec.AppFx.Data.Dapper/Migration/DapperMigrationAttribute.cs
@@ -1,4 +1,5 @@
-﻿using FluentMigrator;
+﻿using System;
+using FluentMigrator;
 
 namespace FluiTec.AppFx.Data.Dapper.Migration
 {
@@ -31,7 +32,7 @@ namespace FluiTec.AppFx.Data.Dapper.Migration
         /// <returns>   The calculated value. </returns>
         private static long CalculateValue(int year, int month, int day, int hour, int minute)
         {
-            return year * 100000000L + month * 1000000L + day * 10000L + hour * 100L + minute;
+            return new DateTime(year, month, day, hour, minute, 0).Ticks;
         }
     }
 }
