09d9ffab51249c677d12c0d3caa4735b5e4e9c89
Achim Schnell <a.schnell@wtschnell.de>
2020-10-28 16:15:04 +0100

    ...

--- src/tests/FluiTec.AppFx.Data.Dapper.Mssql.IntegrationTests/PgsqlTest.cs
+++ src/tests/FluiTec.AppFx.Data.Dapper.Mssql.IntegrationTests/PgsqlTest.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using FluentMigrator.Runner;
+using FluiTec.AppFx.Data.Dapper.Migration;
 using FluiTec.AppFx.Data.Dapper.Pgsql;
 using FluiTec.AppFx.Data.TestLibrary.DataServices;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -10,9 +12,9 @@ namespace FluiTec.AppFx.Data.Dapper.Mssql.IntegrationTests
     [TestCategory("Integration")]
     public class PgsqlTests
     {
-        private readonly bool _isDbAvailable;
-
         private readonly PgsqlDapperServiceOptions _options;
+
+        private bool _isDbAvailable => _options != null;
 
         /// <summary>   Default constructor.</summary>
         public PgsqlTests()
@@ -21,29 +23,39 @@ namespace FluiTec.AppFx.Data.Dapper.Mssql.IntegrationTests
             var usr = Environment.GetEnvironmentVariable("POSTGRES_USER");
             if (!string.IsNullOrWhiteSpace(db) && !string.IsNullOrWhiteSpace(usr))
             {
-                _isDbAvailable = true;
-                _options = new PgsqlDapperServiceOptions {ConnectionString = $"User ID={usr};Host=postgres;Database={db};Pooling=true;" };
+                _options = new PgsqlDapperServiceOptions
+                    {ConnectionString = $"User ID={usr};Host=postgres;Database={db};Pooling=true;"};
             }
+        }
+
+        /// <summary>   Assert database available.</summary>
+        private void AssertDbAvailable()
+        {
+            Assert.IsTrue(_isDbAvailable, "POSTGRES-DB NOT AVAILABLE!");
+        }
+
+        /// <summary>   (Unit Test Method) can check apply migrations.</summary>
+        [TestInitialize]
+        public void CanCheckApplyMigrations()
+        {
+            AssertDbAvailable();
+
+            var service = new PgsqlTestDataService(_options, null);
+            var migrator = new DapperDataMigrator(_options.ConnectionString, new [] {typeof(PgsqlTestDataService).Assembly}, service.MetaData,
+                builder => builder.AddPostgres());
+            migrator.Migrate();
         }
 
         /// <summary>   (Unit Test Method) can create unit of work.</summary>
         [TestMethod]
         public void CanCreateUnitOfWork()
         {
-            if (_isDbAvailable && _options != null)
-            {
-                var service = new PgsqlTestDataService(_options, null);
-                using (var uow = service.BeginUnitOfWork())
-                {
-                    uow.DummyRepository.GetAll();
-                }
+            AssertDbAvailable();
 
-                Console.WriteLine("AVAILABLE");
-            }
-            else
-            {
-                Console.WriteLine("NOT AVAILABLE");
-            }
+            var service = new PgsqlTestDataService(_options, null);
+            var migrator = new DapperDataMigrator(_options.ConnectionString, null, service.MetaData,
+                builder => builder.AddPostgres());
+            migrator.Migrate();
         }
     }
 }
