0886eff5f5c173206bc987f7c682e9128e0d14d3
Achim Schnell <a.schnell@wtschnell.de>
2020-10-28 16:04:10 +0100

    ...

--- src/tests/FluiTec.AppFx.Data.Dapper.Mssql.IntegrationTests/PgsqlTest.cs
+++ src/tests/FluiTec.AppFx.Data.Dapper.Mssql.IntegrationTests/PgsqlTest.cs
@@ -1,15 +1,49 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using FluiTec.AppFx.Data.Dapper.Pgsql;
+using FluiTec.AppFx.Data.TestLibrary.DataServices;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace FluiTec.AppFx.Data.Dapper.Mssql.IntegrationTests
 {
+    /// <summary>   (Unit Test Class) a pgsql tests.</summary>
     [TestClass]
     [TestCategory("Integration")]
     public class PgsqlTests
     {
-        [TestMethod]
-        public void XY()
-        {
+        private readonly bool _isDbAvailable;
 
+        private readonly PgsqlDapperServiceOptions _options;
+
+        /// <summary>   Default constructor.</summary>
+        public PgsqlTests()
+        {
+            var db = Environment.GetEnvironmentVariable("POSTGRES_DB");
+            var usr = Environment.GetEnvironmentVariable("POSTGRES_USER");
+            if (!string.IsNullOrWhiteSpace(db) && !string.IsNullOrWhiteSpace(usr))
+            {
+                _isDbAvailable = true;
+                _options = new PgsqlDapperServiceOptions {ConnectionString = $"User ID={usr};Host=postgres;Database={db};Pooling=true;" };
+            }
+        }
+
+        /// <summary>   (Unit Test Method) can create unit of work.</summary>
+        [TestMethod]
+        public void CanCreateUnitOfWork()
+        {
+            if (_isDbAvailable && _options != null)
+            {
+                var service = new PgsqlTestDataService(_options, null);
+                using (var uow = service.BeginUnitOfWork())
+                {
+                    uow.DummyRepository.GetAll();
+                }
+
+                Console.WriteLine("AVAILABLE");
+            }
+            else
+            {
+                Console.WriteLine("NOT AVAILABLE");
+            }
         }
     }
 }
