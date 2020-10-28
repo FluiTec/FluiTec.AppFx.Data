5fc7b5126f79f5d46463d437aa69769d306033b0
Achim Schnell <a.schnell@wtschnell.de>
2020-10-28 16:45:08 +0100

    simplify unittests

--- src/tests/FluiTec.AppFx.Data.Dapper.Mssql.IntegrationTests/PgsqlTest.cs
+++ src/tests/FluiTec.AppFx.Data.Dapper.Mssql.IntegrationTests/PgsqlTest.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using FluentMigrator.Runner;
+using FluiTec.AppFx.Data.Dapper.DataServices;
 using FluiTec.AppFx.Data.Dapper.Migration;
 using FluiTec.AppFx.Data.Dapper.Pgsql;
 using FluiTec.AppFx.Data.TestLibrary.DataServices;
-using FluiTec.AppFx.Data.TestLibrary.Entities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace FluiTec.AppFx.Data.Dapper.Mssql.IntegrationTests
@@ -11,28 +11,30 @@ namespace FluiTec.AppFx.Data.Dapper.Mssql.IntegrationTests
     /// <summary>   (Unit Test Class) a pgsql tests.</summary>
     [TestClass]
     [TestCategory("Integration")]
-    public class PgsqlTests
+    public class PgsqlTests : DbTest
     {
-        private readonly PgsqlDapperServiceOptions _options;
+        /// <summary>   Gets options for controlling the service.</summary>
+        /// <value> Options that control the service.</value>
+        protected sealed override IDapperServiceOptions ServiceOptions { get; }
 
-        private bool IsDbAvailable => _options != null;
+        /// <summary>   Gets the data service.</summary>
+        /// <value> The data service.</value>
+        protected override ITestDataService DataService { get; }
 
         /// <summary>   Default constructor.</summary>
         public PgsqlTests()
         {
             var db = Environment.GetEnvironmentVariable("POSTGRES_DB");
             var usr = Environment.GetEnvironmentVariable("POSTGRES_USER");
-            if (!string.IsNullOrWhiteSpace(db) && !string.IsNullOrWhiteSpace(usr))
-            {
-                _options = new PgsqlDapperServiceOptions
-                    {ConnectionString = $"User ID={usr};Host=postgres;Database={db};Pooling=true;"};
-            }
-        }
 
-        /// <summary>   Assert database available.</summary>
-        private void AssertDbAvailable()
-        {
-            Assert.IsTrue(IsDbAvailable, "POSTGRES-DB NOT AVAILABLE!");
+            if (string.IsNullOrWhiteSpace(db) || string.IsNullOrWhiteSpace(usr)) return;
+
+            ServiceOptions = new PgsqlDapperServiceOptions
+            {
+                ConnectionString = $"User ID={usr};Host=postgres;Database={db};Pooling=true;"
+            };
+
+            DataService = new PgsqlTestDataService(ServiceOptions, null);
         }
 
         /// <summary>   (Unit Test Method) can check apply migrations.</summary>
@@ -41,36 +43,9 @@ namespace FluiTec.AppFx.Data.Dapper.Mssql.IntegrationTests
         {
             AssertDbAvailable();
 
-            var service = new PgsqlTestDataService(_options, null);
-            var migrator = new DapperDataMigrator(_options.ConnectionString, new [] {typeof(PgsqlTestDataService).Assembly}, service.MetaData,
+            var migrator = new DapperDataMigrator(ServiceOptions.ConnectionString, new [] {DataService.GetType().Assembly}, ((IDapperDataService)DataService).MetaData,
                 builder => builder.AddPostgres());
             migrator.Migrate();
-        }
-
-        /// <summary>   (Unit Test Method) can create unit of work.</summary>
-        [TestMethod]
-        public void CanCreateUnitOfWork()
-        {
-            AssertDbAvailable();
-
-            var service = new PgsqlTestDataService(_options, null);
-            using (var uow = service.BeginUnitOfWork())
-            {
-            }
-        }
-
-        /// <summary>   (Unit Test Method) can create entity.</summary>
-        [TestMethod]
-        public void CanCreateEntity()
-        {
-            AssertDbAvailable();
-
-            var service = new PgsqlTestDataService(_options, null);
-            using (var uow = service.BeginUnitOfWork())
-            {
-                var entity = uow.DummyRepository.Add(new DummyEntity {Name = "Test"});
-                Assert.IsTrue(entity.Id > -1);
-            }
         }
     }
 }
