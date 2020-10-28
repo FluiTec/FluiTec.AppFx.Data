afb10ff60b022e7079a912fd59c42714f250fef4
Achim Schnell <a.schnell@wtschnell.de>
2020-10-28 15:44:29 +0100

    test ci with env-variables

---
+++ src/tests/FluiTec.AppFx.Data.Dapper.Mssql.IntegrationTests/PgsqlTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FluiTec.AppFx.Data.Dapper.Mssql.IntegrationTests
+{
+    [TestClass]
+    [TestCategory("Integration")]
+    public class PgsqlTests
+    {
+        [TestMethod]
+        public void XY()
+        {
+
+        }
+    }
+}
