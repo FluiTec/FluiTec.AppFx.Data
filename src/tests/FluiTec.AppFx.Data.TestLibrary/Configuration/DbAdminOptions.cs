8eb5305ced99b450dc2c9651f31d5a9d3c9d260d
Achim Schnell <a.schnell@wtschnell.de>
2021-10-07 13:04:10 +0200

    ...

--- src/tests/FluiTec.AppFx.Data.TestLibrary/Configuration/DbAdminOptions.cs
+++ src/tests/FluiTec.AppFx.Data.TestLibrary/Configuration/DbAdminOptions.cs
@@ -2,9 +2,15 @@
 {
     public class DbAdminOptions
     {
+        private string _adminConnectionString;
+
         /// <summary>   Gets or sets the server.</summary>
         /// <value> The server.</value>
-        public string AdminConnectionString { get; set; }
+        public string AdminConnectionString
+        {
+            get => string.IsNullOrWhiteSpace(_adminConnectionString) ? null : _adminConnectionString;
+            set => _adminConnectionString = value;
+        }
 
         /// <summary>   Gets or sets the integration database.</summary>
         /// <value> The integration database.</value>
