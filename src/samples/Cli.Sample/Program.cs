22d9972ae7f9ea23c544b87275da78b8a72bb2e4
Achim Schnell <a.schnell@wtschnell.de>
2022-02-04 08:11:57 +0100

    cleanup

--- src/samples/Cli.Sample/Program.cs
+++ src/samples/Cli.Sample/Program.cs
@@ -85,32 +85,6 @@ internal class Program : ValidatingConfigurationManagerProgram
             };
         });
 
-        //services.ConfigureDynamicDataProvider(Manager,
-        //    new Func<IOptionsMonitor<DynamicDataOptions>, IServiceProvider, ITestDataService>((options, provider) =>
-        //        {
-        //            return options.CurrentValue.Provider switch
-        //            {
-        //                DataProvider.LiteDb => new LiteDbTestDataService(
-        //                    provider.GetRequiredService<IOptionsMonitor<LiteDbServiceOptions>>(),
-        //                    provider.GetService<ILoggerFactory>()),
-        //                DataProvider.Mssql => new MssqlTestDataService(
-        //                    provider.GetRequiredService<IOptionsMonitor<MssqlDapperServiceOptions>>(),
-        //                    provider.GetService<ILoggerFactory>()),
-        //                DataProvider.Pgsql => new PgsqlTestDataService(
-        //                    provider.GetRequiredService<IOptionsMonitor<PgsqlDapperServiceOptions>>(),
-        //                    provider.GetService<ILoggerFactory>()),
-        //                DataProvider.Mysql => new MysqlTestDataService(
-        //                    provider.GetRequiredService<IOptionsMonitor<MysqlDapperServiceOptions>>(),
-        //                    provider.GetService<ILoggerFactory>()),
-        //                DataProvider.Sqlite => new SqliteTestDataService(
-        //                    provider.GetRequiredService<IOptionsMonitor<SqliteDapperServiceOptions>>(),
-        //                    provider.GetService<ILoggerFactory>()),
-        //                _ => throw new NotImplementedException()
-        //            };
-        //        }
-        //    )
-        //);
-
         services.ConfigureOptionsConsoleModule();
         services.ConfigureDataConsoleModule();
 
