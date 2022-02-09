726e0c2195846987f8415ce3cd86650aff7351fe
Achim Schnell <a.schnell@wtschnell.de>
2022-02-09 15:12:50 +0100

    add more extensions

--- src/FluiTec.AppFx.Data.Dapper/Extensions/DapperMigrationExtension.cs
+++ src/FluiTec.AppFx.Data.Dapper/Extensions/DapperMigrationExtension.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using FluentMigrator.Builders.Alter;
+using FluentMigrator.Builders.Alter.Table;
 using FluentMigrator.Builders.Create;
 using FluentMigrator.Builders.Create.Table;
 using FluentMigrator.Builders.Delete;
+using FluentMigrator.Builders.Delete.Column;
 
 namespace FluiTec.AppFx.Data.Dapper.Extensions;
 
@@ -40,5 +43,42 @@ public static class DapperMigrationExtension
                 .InSchema(schemaName);
         else
             expressionRoot.Table($"{schemaName}_{tableName}");
+    }
+
+    /// <summary>
+    /// An IDeleteColumnFromTableSyntax extension method that from table.
+    /// </summary>
+    ///
+    /// <param name="expressionRoot">   The expressionRoot to act on. </param>
+    /// <param name="schemaName">       Name of the schema. </param>
+    /// <param name="tableName">        Name of the table. </param>
+    /// <param name="supportsSchema">   True to supports schema. </param>
+    public static void FromTable(this IDeleteColumnFromTableSyntax expressionRoot,
+        string schemaName, string tableName, bool supportsSchema)
+    {
+        if (supportsSchema)
+            expressionRoot
+                .FromTable(tableName)
+                .InSchema(schemaName);
+        else
+            expressionRoot.FromTable($"{schemaName}_{tableName}");
+    }
+
+    /// <summary>
+    /// An ICreateExpressionRoot extension method that tables.
+    /// </summary>
+    ///
+    /// <param name="expressionRoot">   The expressionRoot to act on. </param>
+    /// <param name="schemaName">       Name of the schema. </param>
+    /// <param name="tableName">        Name of the table. </param>
+    /// <param name="supportsSchema">   True to supports schema. </param>
+    public static IAlterTableAddColumnOrAlterColumnSyntax Table(this IAlterExpressionRoot expressionRoot,
+        string schemaName, string tableName, bool supportsSchema)
+    {
+        return supportsSchema
+            ? expressionRoot
+                .Table(tableName)
+                .InSchema(schemaName)
+            : expressionRoot.Table($"{schemaName}_{tableName}");
     }
 }
