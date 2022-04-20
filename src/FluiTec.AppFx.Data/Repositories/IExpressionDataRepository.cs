3993e4e097b2ca39c50a0cd7c8fa890114828efe
Achim Schnell <a.schnell@wtschnell.de>
2022-04-20 15:58:22 +0200

    Add IExpressionDataRepository

---
+++ src/FluiTec.AppFx.Data/Repositories/IExpressionDataRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using FluiTec.AppFx.Data.Entities;
+
+namespace FluiTec.AppFx.Data.Repositories;
+
+/// <summary>
+/// Interface for expression data repository.
+/// </summary>
+///
+/// <typeparam name="TEntity">  Type of the entity. </typeparam>
+public interface IExpressionDataRepository<TEntity> : IDataRepository<TEntity>
+    where TEntity : class, IEntity, new()
+{
+    /// <summary>
+    /// Enumerates the items in this collection that meet given criteria.
+    /// </summary>
+    ///
+    /// <param name="expression">   The expression. </param>
+    ///
+    /// <returns>
+    /// An enumerator that allows foreach to be used to process the matched items.
+    /// </returns>
+    IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> expression);
+}
