d2c656171eaf43a7dec262dc22ad6d5297142c38
Achim Schnell <a.schnell@wtschnell.de>
2022-05-16 14:58:31 +0200

    ...

---
+++ src/FluiTec.AppFx.Data.Migration/NameGenerators/ForeignKeyIndexNameGenerator.cs
@@ -0,0 +1,21 @@
+namespace FluiTec.AppFx.Data.Migration.NameGenerators;
+
+/// <summary>
+/// Name generator for forein keys.
+/// </summary>
+public static class ForeignKeyIndexNameGenerator
+{
+    private const string Prefix = "FK";
+
+    /// <summary>
+    /// Creates a new name for a foreign key.
+    /// </summary>
+    /// <param name="schema">       Schema to use.</param>
+    /// <param name="tableTo">      Table that contains the primary key to reference.</param>
+    /// <param name="tableFrom">    Table that uses a foreign key reference.</param>
+    /// <returns></returns>
+    public static string CreateName(string schema, string tableTo,string tableFrom)
+    {
+        return $"{Prefix}_{schema}_{tableTo}_{tableFrom}";
+    }
+}
